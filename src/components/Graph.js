cfe47dc8adddbde711ffddea2edd7078f92d0bdf
cd-x <whowantsthisemailid@gmail.com>
2022-10-19 21:25:36 +0530

    eliminate hard coded edge

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,45 +1,43 @@
-import { useState } from "react";
-import { Layer, Circle } from "react-konva";
+import { Layer, Group } from "react-konva";
 import Edge from "./Edge";
 import Vertex from "./Vertex";
 
-const BLUE1_DEFAULTS = {
-  x: 100,
-  y: 100,
-  fill: "blue",
-  width: 30,
-  height: 30,
-  draggable: true,
-};
-
-const BLUE2_DEFAULTS = {
-  x: 100,
-  y: 300,
-  fill: "blue",
-  width: 30,
-  height: 30,
-  draggable: true,
-};
+const EDGES = [
+  [
+    {
+      id: 0,
+      x: 100,
+      y: 100,
+    },
+    {
+      id: 1,
+      x: 100,
+      y: 300,
+    },
+  ],
+];
 
 const Graph = (props) => {
-  const [blue1Node, setBlue1Node] = useState(BLUE1_DEFAULTS);
-  const [blue2Node, setBlue2Node] = useState(BLUE2_DEFAULTS);
   return (
     <Layer>
-      <Edge node1={blue2Node} node2={blue1Node} />
-      <Circle
-        {...blue1Node}
-        onDragMove={(e) => {
-          setBlue1Node({ ...blue1Node, ...e.target.position() });
-        }}
-      />
-      <Circle
-        {...blue2Node}
-        onDragMove={(e) => {
-          setBlue2Node({ ...blue2Node, ...e.target.position() });
-        }}
-      />
-      <Vertex label="23" xStart={150} yStart={300} />
+      {EDGES.map((edge) => {
+        const [firstNode, secondNode] = edge;
+        return (
+          <Group>
+            <Edge node1={firstNode} node2={secondNode} />
+            <Vertex
+              label={firstNode.id}
+              xStart={firstNode.x}
+              yStart={firstNode.y}
+            />
+            <Vertex
+              label={secondNode.id}
+              xStart={secondNode.x}
+              yStart={secondNode.y}
+            />
+          </Group>
+        );
+      })}
     </Layer>
   );
 };
