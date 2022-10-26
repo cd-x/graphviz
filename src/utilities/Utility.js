ee0de243df4c0e71d8767bc309cf3931532123e8
cd-x <whowantsthisemailid@gmail.com>
2022-10-26 18:19:52 +0530

    remove invert-color lib

--- src/utilities/Utility.js
+++ src/utilities/Utility.js
@@ -1,12 +1,1 @@
-import invert from "invert-color";
-
 export const degrees_to_radians = (degrees) => degrees * (Math.PI / 180);
-
-export const randomColor = () => {
-  const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  const invertedColor = invert(color);
-  return {
-    backgroundColor: color,
-    foregroundColor: invertedColor,
-  };
-};
