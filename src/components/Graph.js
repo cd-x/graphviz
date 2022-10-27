e459268119e1e3c760cc6e7f0ee831483eae2ab0
cd-x <whowantsthisemailid@gmail.com>
2022-10-27 17:10:47 +0530

    removed hardcoded nodes & edges

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -2,43 +2,12 @@ import { useState } from "react";
 import { Layer } from "react-konva";
 import Edge from "./Edge";
 import Vertex from "./Vertex";
-
-const WINDOW_WIDTH = window.innerWidth - 30;
-const WINDOW_HEIGHT = window.innerHeight - 30;
-
-const VERTICES = (function () {
-  const number = 10;
-  let result = [];
-  while (result.length < number) {
-    result.push({
-      id: "V-" + result.length,
-      x: WINDOW_WIDTH * Math.random(),
-      y: WINDOW_HEIGHT * Math.random(),
-    });
-  }
-  return result;
-})();
-
-const EDGES = (function () {
-  const number = 10;
-  let result = [];
-  while (result.length < number) {
-    var from = "V-" + Math.floor(Math.random() * VERTICES.length);
-    var to = "V-" + Math.floor(Math.random() * VERTICES.length);
-    if (from === to) {
-      continue;
-    }
-    result.push({
-      id: "connector-" + result.length,
-      from: from,
-      to: to,
-    });
-  }
-  return result;
-})();
+import { useSelector } from "react-redux";
 
 const Graph = (props) => {
-  const [points, setPoints] = useState(VERTICES);
+  const vertices = useSelector((state) => state.graph.vertices);
+  const edges = useSelector((state) => state.graph.edges);
+  const [points, setPoints] = useState(vertices);
   const vertexMoveHandler = ({ id, x, y }) => {
     setPoints((prevPoints) => {
       const idx = prevPoints.findIndex((pp) => pp.id === id);
@@ -49,7 +18,7 @@ const Graph = (props) => {
   };
   return (
     <Layer>
-      {EDGES.map((edgeItem) => {
+      {edges.map((edgeItem) => {
         const { id, from, to } = edgeItem;
         const fromNode = points.find((p) => p.id === from);
         const toNode = points.find((p) => p.id === to);
